7963ede9b4cd9bef2eccfbdb5a52b680e7d9c387
ducanh2609 <ducanh.nl2@gmail.com>
2022-11-29 17:32:15 +0700

    Project Sell

--- js/view.js
+++ js/view.js
@@ -195,6 +195,7 @@ view.setScreenActive = (screenName) => {
                     mainShowSearch.style.visibility = "hidden";
                     productList.style.display = "block";
                     mainShowSearch.innerHTML = "";
+                    location.reload()
                 }
             })
             search.addEventListener("input", () => {
